02e1f4828f554f1ea9cca9dd8f803dddb57496a4
Daiego43 <diedelcha@gmail.com>
2024-02-16 00:21:02 +0100

    fixing setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     python_requires='>=3.10',
     entry_points={
         'console_scripts': [
-            'addu = ADDU.__main__:main'
+            'addu=ADDU.__main__:main'
         ],
     },
 )
